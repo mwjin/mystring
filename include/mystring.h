8b7ac2df3625178dee0354318d6f025a843e9e83
Minwoo Jeong <jeongmwj@gmail.com>
2023-07-17 19:29:10 +0900

    Update the declaration
    - Declare a constructor for empty strings
    - Correct the copy constructor declaration
    - Add getters

--- include/mystring.h
+++ include/mystring.h
@@ -1,15 +1,18 @@
-
 #include <cstddef>
 
 class MyString {
  public:
+  MyString();
   MyString(char c);
-  MyString(char* s);
+  MyString(const char* s);
   MyString(const MyString& other);
   ~MyString();
-  MyString& operator=(const MyString* other);
+  MyString& operator=(const MyString& other);
+
+  const char* str() const;
+  int length() const;
 
  private:
-  char* str_;
-  std::size_t len_;
+  char* str_ = nullptr;
+  std::size_t len_ = 0;
 };
